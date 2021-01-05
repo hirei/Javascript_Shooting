3e3139a562ec6591dfbbe39563bf4a64a2250837
hirei <activeage03@icloud.com>
2021-01-05 09:53:23 +0900

    second_commit

--- javascript.js
+++ javascript.js
@@ -15,14 +15,14 @@
 
 　　class Enemy {
     　constructor(x,y,angle,kind){
-        this.kind = kind;
+        this.kind = kind;   //敵の種類
         this.BULLET_NUM = 3;
-        this.bullet = Array(this.BULLET_NUM);
-	     
+        this.bullet = Array(this.BULLET_NUM);   //敵の弾の配列
+
         for(var i = 0;i < 5;i++) {
-		　　this.bullet[i] = new Bullet();
+		　　this.bullet[i] = new Bullet();  //弾を生成
 	}
-	     
+
 	this.x = x; // X座標
 	this.y = y; // Y座標
 	this.width = 32; // 幅
@@ -34,25 +34,25 @@
 
     	getBulletNum(){
         	for(var i=0; i<this.BULLET_NUM; i++){
-            	　　if(!this.bullet[i].exist){
-                　　return i;
+            	　　if(!this.bullet[i].exist){      //この球が存在しないなら
+                　　return i;       //iを返す
             　　　}
         }
-        return -1;
+        return -1;  //異常終了を表す(関数が失敗している)
     }
 	  
     shot() {
 		if(this.cnt % 20 == 0) { // 20カウントずつ発射
 			var num = this.getBulletNum(); // 発射されてない弾の番号を取得
-			if(num != -1) {
+			if(num != -1) {     //異常終了の値ではないなら
 				//	弾を登録
 				this.bullet[num].enter(this.x, this.y, 4, 4, this.angle, 5);
 			}
 		}
 		//	発射された弾を更新
 		for(var i = 0;i < this.BULLET_NUM;i++) {
-			if(this.bullet[i].exist) {
-				this.bullet[i].move();
+			if(this.bullet[i].exist) {  //この弾が存在するなら
+				this.bullet[i].move();  //弾を動かす(発射)
 			}
 		}
 		//	カウンタを更新
@@ -63,31 +63,34 @@
 
     move(){
         this.x += Math.cos(this.angle) * this.spd;
-		this.y += Math.sin(this.angle) * this.spd;
+        this.y += Math.sin(this.angle) * this.spd;
+        var rn = Math.floor(Math.random() * (3 - 1) + 1);   //1~3の間のランダムな値
 		
         //	壁に当たったら跳ね返る
 		if(this.x < this.width / 2 || this.x > canvas.width - this.width / 2) {
-			var r = this.angle - Math.PI / 2;
+			var r = this.angle - Math.PI / rn;
 			this.angle = this.angle - 2 * r;
-			this.spd *= 1.01;
+			this.spd *= 1.005;
 		}
 		else if(this.y < this.height / 2 || this.y > canvas.height - this.height / 2) {
-			var r = this.angle - Math.PI * 2;
+			var r = this.angle - Math.PI * rn;
 			this.angle = this.angle - 2 * r;
-			this.spd *= 1.01;
+			this.spd *= 1.005;
 		}
     }
+    //描画
     draw(ctx){
-        switch(this.kind){
+        switch(this.kind){  //敵の種類の描画
             case 0: ctx.fillStyle = "rgb(0, 255, 255)"; break; // 水色
 			case 1: ctx.fillStyle = "rgb(0, 255, 0)"; break; // 緑色
 			case 2: ctx.fillStyle = "rgb(255, 255, 0)"; break; // 黄色
         }
+        //短形の描画
         ctx.fillRect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
-
+        //弾の描画
         for(var i=0; i< this.BULLET_NUM; i++){
-            if(this.bullet[i].exist){
-                this.bullet[i].draw(ctx);
+            if(this.bullet[i].exist){       //存在するなら
+                this.bullet[i].draw(ctx);   //描画する
             }
         }
     }
@@ -113,20 +116,19 @@ class Bullet {
         this.angle= angle;
         this.spd= spd;
     }
-    
+    //動作
     move(){
         this.x += Math.cos(this.angle)*this.spd;
         this.y += Math.sin(this.angle)*this.spd;
 
         if(this.x<0 || this.x > canvas.width || this.y <0 || this.y> canvas.height){
-            this.exist = false;
-        } 
+            this.exist = false;     //弾の存在を消す
+        }
     }
-
+    //描画
     draw(ctx){
         ctx.fillStyle= "yellow";
         ctx.fillRect(this.x - (this.width / 2), this.y - (this.height / 2), this.width, this.height);
-
     }
 }
 
@@ -134,12 +136,12 @@ class Bullet {
 class Player{
     constructor(){
         this.cnt =0;
-        this.residue = 3;
-        this.deffect = false;
-        this.x = canvas.width/2;
-        this.y = canvas.height*3/4;
-        this.width = 24;
-        this.height = 36;
+        this.residue = 3;   //体力
+        this.deffect = false;   //当たり判定
+        this.x = canvas.width/2;    //横座標
+        this.y = canvas.height*3/4; //縦座標
+        this.width = 24;    //横の大きさ
+        this.height = 36;   //縦の大きさ
 
         this.BULLET_NUM =5;
         this.bullet =Array(this.BULLET_NUM);
@@ -153,13 +155,13 @@ class Player{
         var hori =false;
         var vert =false;
 
-        if(key[KEY_RIGHT] == 1 || key[KEY_LEFT] == 1){ //横移動
+        if(key[KEY_RIGHT] == 1 || key[KEY_LEFT] == 1){  //横移動
             hori =true;
         }
-        if(key[KEY_UP] == 1 || key[KEY_DOWN] == 1) {
+        if(key[KEY_UP] == 1 || key[KEY_DOWN] == 1) {    //縦移動
 			vert = true;
 		}
-        if(hori && vert){
+        if(hori && vert){   //斜め移動
             diagonal = Math.sqrt(2.0); //√２
         }
 
@@ -175,7 +177,7 @@ class Player{
 		}
 
     }
-
+    //描画
     draw(ctx) {
         if(!(this.deffect && this.cnt % 2 ==0)){
         ctx.fillStyle = "red";
@@ -183,7 +185,7 @@ class Player{
         }
 
         for(var i = 0;i < this.BULLET_NUM;i++) {
-			if(this.bullet[i].exist) {
+			if(this.bullet[i].exist) {      //存在するなら
 				this.bullet[i].move();
 				this.bullet[i].draw(ctx);
 			}
@@ -198,18 +200,18 @@ class Player{
 
     getBulletNum(){
         for(var i=0; i<this.BULLET_NUM; i++){
-            if(!this.bullet[i].exist){
-                return i;
+            if(!this.bullet[i].exist){  //存在しないなら
+                return i;   //iを返す
             }
         }
-        return -1;
+        return -1;  //異常終了(関数がうまくいってない)
     }
 
     shot(key){
         var num;
-        if(key[KEY_Z] ==1){
+        if(key[KEY_Z] ==1){     //Zキーが押されたなら
             num =this.getBulletNum();
-            if(num != -1){
+            if(num != -1){  //-1(異常終了の値)でないなら
                 this.bullet[num].enter(this.x,this.y, 4, 36, -Math.PI/2,10);
                 key[KEY_Z]++;
             }
@@ -235,6 +237,7 @@ requestAnimationFrame(main);
 
 function main(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+    window.isKeyDown = {};
 
     if(!gameover){
     player.shot(key);
@@ -242,8 +245,8 @@ function main(){
     player.draw(ctx);
     }
     for(var i = 0;i < ENEMY_NUM;i++) {
-        enemy[i].shot();
-		enemy[i].move(); // 敵を移動
+        enemy[i].shot();    // 敵が撃つ
+		enemy[i].move();    // 敵を移動
 		enemy[i].draw(ctx); // 敵を描画
 	}
     if(!gameover){
@@ -293,15 +296,15 @@ function main(){
             }
         }
     }
-
+    //残機が0になったら
     if(player.residue ==0){
-        gameover = true;
+        gameover = true;    //gameoverを起動する
         for(var i=0;i<player.BULLET_NUM;i++){
-            player.bullet[i].exist = false;
+            player.bullet[i].exist = false;     //playerの弾の存在を消す
         }
     }
 
-
+    //プレイヤーの残機を表示
     for(var i=0; i<player.residue;i++){
         ctx.fillStyle = "rgb(255, 0, 0)";
 		ctx.fillRect(10 + i * 40, 60, player.width, player.height);
